baf852232cc54379ab9c3e55ef950f52cf2d41fb
Inshra23 <164750803+Inshra23@users.noreply.github.com>
2024-05-06 16:35:41 +0500

    mypractice

---
+++ script.js
@@ -0,0 +1,81 @@
+console.log("Hello World");
+console.log("inshra abid");
+document.write("Hello Javascript")
+
+alert("This is not working")
+alert("Welcome to our website!");
+console.log("careful writer ends every declaration sentence with a period");
+var first_name = "Inshra";
+var first_name;  //declare
+first_name = "inshra";
+var first_name = "inshra"
+console.log(first_name);
+var last_name = "Shah";
+console.log(last_name);
+
+var text = "It is invented in 2021";
+console.log(text);
+
+var name = "Inshra Shah";
+var email ="inshraabid4@gmail.com"
+var dateOfBirth = 2006;
+console.log("my name is" + name + " my email is " + email + " my date of birth is " + dateOfBirth);
+
+var message = "Hello in Javascript World";
+alert(message)
+
+var Hello;
+console.log(Hello); //Undefined
+
+var lastname = "Inshra";
+var lastname = "shah";
+console.log(lastname);
+
+
+
+
+console.log(email);
+console.log(dateOfBirth);
+let a = 12;
+let b = 16;
+console.log("The addition of two number is: " + (a + b));
+console.log("The multiplication of two number is: " + (a * b));
+console.log("The subraction of two number is: " + (a - b));
+console.log("The division of two number is: " + (a / b));
+console.log("The modulus of two number is: " + (a % b));
+
+//legal Variable
+let FirstName;
+let LastName;
+let lastname;
+let _dateofbirth;
+let $email;
+let Age;
+
+
+let rose = "flower"
+console.log(Rose);
+
+let onevar = "one"; secondvar = "second"; thirdvar = "third"
+//Question2
+//Legal Variables
+let firstName = "Inshra"
+let secondname = "abid"
+let age = "17"
+let email = "inshraabid4@gmail.com"
+let -dateOfBirth
+
+
+
+
+//Illegal Variable
+
+
+
+//Question 5
+let costOfTicket = 600;
+let buyingTicket = 5;
+let result = costOfTicket + buyingTicket;
+console.log("totalcost");
+
+
